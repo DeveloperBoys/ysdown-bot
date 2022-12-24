46d700e5e8b3b56bb03c5b11d4aeee565ec2afe3
Developerboys <murtazoxurramov1@gmail.com>
2022-12-24 15:16:29 +0500

    fixed bugs

--- handlers/user/commands.py
+++ handlers/user/commands.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from loader import dp
 from aiogram import types
 from decouple import config
@@ -10,11 +11,16 @@ AUTH_TOKEN = config('TOKEN')
 @dp.message_handler()
 async def post_url(message: types.Message):
     url = message.text
-    print(url)
     head = {'Authorization': 'token {}'.format(AUTH_TOKEN)}
     try:
         response = requests.post(url=f'{BASE_URL}/api/v1/url/', headers=head, data={
             'original_url': url
         })
-    except:
-        print('Error!')
+        if response.status_code != 404:
+            await message.reply('URL muvoffaqiyatli yuborildi!')
+            data = json.loads(response.content)
+            print(data)
+        else:
+            await message.reply(f"Uzr nimadur xato - {response.status_code}")
+    except requests.HTTPError or requests.ConnectionError as er:
+        print(er)
